53e5e790a8c7701756cd348213b9d0f842997ec7
anikishov <gryz9ns@gmail.com>
2020-09-29 22:16:14 +0300

    some changes

--- send_temp.py
+++ send_temp.py
@@ -1,9 +1,33 @@
 #!/opt/me/env/bin/python3
 
 import os, requests
-from flask import Flask
+from flask import Flask,request
 
 app = Flask(__name__)
+
+'''
+This route and function will set wireguard interface
+'''
+def set_gw(iface):
+    result = os.popen(f'systemctl start wg-quick@{iface}').read()
+    return result
+
+def get_gw():
+    result = os.popen('wg').read()
+    return result
+
+@app.route('/gw', methods = ['POST','GET'])
+def gw():
+    if request.method == 'POST':
+        data = request.get_json(force=True)
+        if data['iface']:
+            iface = data['iface']
+            result = set_gw(iface)
+            return result, 200
+        else:
+            return data, 400
+    elif request.method == 'GET':
+        return get_gw()
 
 @app.route('/temp', methods = ['GET'])
 def give_temp():
